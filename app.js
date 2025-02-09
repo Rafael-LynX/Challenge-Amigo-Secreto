a5bded6e218c77152bece40a2a1682e2f81f6eb8
Rafael Vieira <109623407+Rafael-LynX@users.noreply.github.com>
2025-02-09 12:07:55 -0300

    Implementando a função listaAmigos, para exibir os amigos da lista adicionados

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ let amigos = [];
 
 
 // Função para adicionar um amigo na lista/array (amigos)
+// Validando a entrada do usuário, evitando campos vazios sejam adicionados
+// E adicionando um nome válido na lista de amigos
 function adicionarAmigo() {
     let amigo = sanitizarEntrada(document.getElementById('amigo').value.trim());
 
-    // validação da entrada do usuário
     if (amigo.trim() !== '') {
         amigos.push(amigo);
         limparCampo();
-        exibirTextoNaTela('h2', `Amigo adicionado com sucesso ${amigo}`);
+        exibirTextoNaTela('h2', `Amigo adicionado com sucesso: ${amigo}`);
+        listaAmigos();
         console.log(amigos);
     } else {
         exibirTextoNaTela('h2', 'Por favor, insira um nome válido');
@@ -19,9 +21,31 @@ function adicionarAmigo() {
 
     }
 } 
+
+
 // Função para sanitizar/limpar a entrada do usuário
+// Evitando caracteres especiais e números
 function sanitizarEntrada(entrada) {
     return entrada.replace(/[^a-zA-Z\s]/g, '');
+}
+
+// Função para exibir a lista de amigos na tela
+// Utilizando o ID listaAmigos do HTML e limpando ele para adicionar os novos amigos
+// Ele itera cada elemento na lista de amigos e cria um elemento li no HTML
+// Com cada amigo adicionado
+function listaAmigos(){
+    let amigosLista = document.getElementById('listaAmigos');
+    amigosLista.innerHTML = '';
+    amigos.forEach((amigo) => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        amigosLista.appendChild(li);
+    });
+}
+
+
+function sortearAmigo(){
+
 }
 
 // Função para exibir os informações na tela
@@ -31,7 +55,7 @@ function exibirTextoNaTela(tag, texto){
 }
 
 
-// Função para limpar o campo de input
+// Função para limpar o campo de input logo após a digitação
 function limparCampo() {
     limpar = document.querySelector('input');
     limpar.value = '';
